e1234aef3be890c7da4c2edfc98c4e909642ea3e
klealufide <kleal40321@ufide.ac.cr>
2025-06-16 20:16:55 -0600

    condicionales

--- index.php
+++ index.php
@@ -58,7 +58,61 @@
             break;
     }
 
+    $a = 5;
+    $b = "5";
 
+    echo $espacio;
+    // comparar solo valor 
+
+    if ($a ==  $b) {
+        echo "El valor de a y b es igual" . $espacio;
+    }
+
+    if ($a === $b) {
+        echo "El valor y tipo de a y b es igual" . $espacio;
+    }
+
+    if ($a != $b) {
+        echo "Los valores de a y b son diferentes" . $espacio;
+    }
+
+    if ($a !== $b) {
+        echo "el valor o el tipo son diferentes" . $espacio;
+    }
+
+    $edad = "8";
+
+    if ($edad < $edadMayor) {
+        echo "Es menor de edad";
+    }
+
+    if ($edad >= $edadMayor) {
+        echo "Es mayor de edad";
+    }
+
+    $membresia = false;
+
+    if ($membresia && $edad >= $edadMayor) {
+        echo "Es adulto con membresia" . $espacio;
+    } elseif ($membresia && $edad < $edadMayor) {
+        echo "Es menor de edad con membresia" . $espacio;
+    } else {
+        if ($edad >= $edadMayor) {
+            echo "Es adulto sin membresia" . $espacio;
+        } else {
+            echo "Es menor de edad sin membresia" . $espacio;
+        }
+    }
+
+    if ($membresia || $edad < $edadMayor) {
+        echo "Tiene membresia o es menor edad" . $espacio;
+    } else  {
+         echo "No tiene membresia o no es menor edad" . $espacio;
+    }
+
+    if(!$membresia){
+        echo "La persona tiene membresia" . $espacio;
+    }
     ?>
 </body>
 
