17a5f41e46b3eccaa1634928517500f95430fd31
klealufide <kleal40321@ufide.ac.cr>
2025-06-16 18:40:28 -0600

    Primercambio

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Semana 06</title>
+</head>
+
+<body>
+    <h1>Semana 06</h1>
+    <?php
+        echo "<h2>Clases de php </h2>";
+
+    ?>
+</body>
+
+</html>
